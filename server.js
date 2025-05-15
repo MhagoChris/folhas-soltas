cc047aabe0a225738b133fdc0ffba0322e105708
MhagoChris <bce.producoes@gmail.com>
2025-05-15 09:34:21 -0400

    Remoção de arquivos manuais e inclusão de páginas dinâmicas

--- server.js
+++ server.js
@@ -54,6 +54,71 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Seção Histórias
+app.get('/historias.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'historias.html'));
+});
+
+// Página individual de leitura de história
+app.get('/historia.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'historia.html'));
+});
+
+// Seção Notícias
+app.get('/noticias.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'noticias.html'));
+});
+
+// Página individual de notícia
+app.get('/noticia.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'noticia.html'));
+});
+
+// Página individual Vídeo
+app.get('/video.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'video.html'));
+});
+
+// Página individual de livro
+app.get('/livro.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'livro.html'));
+});
+
+// Seção Autores
+app.get('/autores.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'autores.html'));
+});
+
+// Seção Autores
+app.get('/autores.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'autores.html'));
+});
+
+// Seção Sobre
+app.get('/sobre.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'sobre.html'));
+});
+
+// Página Todas as Notícias
+app.get('/todas-noticias.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'todas-noticias.html'));
+});
+
+// Seção Contato
+app.get('/contato.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contato.html'));
+});
+
+// Página Administração
+app.get('/admin.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'admin.html'));
+});
+
+// Página Editor
+app.get('/editor.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'editor.html'));
+});
+
 app.listen(port, () => {
   console.log(`✅ Servidor rodando em http://localhost:${port}`);
 });
